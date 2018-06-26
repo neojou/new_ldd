ca63ed62467d158330faef7f1a9fda175c54fb32
Ji-Pin Jou <neo_jou@realtek.com>
2018-06-26 10:32:31 +0800

    work queue
    work queue example

---
+++ work_queue/src/work_queue.c
@@ -0,0 +1,58 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>	/* for sleep */
+#include <linux/time.h>
+#include <linux/delay.h>
+#include <linux/slab.h>		/* for kmalloc */
+#include <linux/workqueue.h>
+
+
+struct workqueue_struct *wq;
+
+struct work_data {
+	struct work_struct my_work;
+	int the_data;
+};
+
+static void work_handler(struct work_struct *work)
+{
+	struct work_data *my_data = container_of(work, struct work_data,
+						 my_work);
+
+	pr_info("%s: data is %d\n", __func__, my_data->the_data);
+	msleep(2000);
+	kfree(my_data);
+	pr_info("%s: bye\n", __func__);
+}
+
+static int __init hello_init(void)
+{
+	struct work_data *my_data;
+
+	wq = create_singlethread_workqueue("my_single_thread");
+	my_data = kmalloc(sizeof(struct work_data), GFP_KERNEL);
+	my_data->the_data = 666;
+
+	INIT_WORK(&my_data->my_work, work_handler);
+	queue_work(wq, &my_data->my_work);
+	pr_info("%s: bye\n", __func__);
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	flush_workqueue(wq);
+	destroy_workqueue(wq);
+	pr_info("%s: bye\n", __func__);
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
+
+MODULE_AUTHOR("Neo Jou <neojou@gmail.com>");
+MODULE_AUTHOR("John Madieu <john.madieu@gmail.com");
+MODULE_LICENSE("GPL");
