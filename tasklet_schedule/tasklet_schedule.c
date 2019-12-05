19ba862cff69786439e96675f3543710f8baebd1
neo_jou <neo_jou@realtek.com>
2019-12-05 13:17:46 +0800

    new_ldd : tasklet_schedule     tasklet is a kind of softirq
    Signed-off-by: neo_jou <neo_jou@realtek.com>

--- tasklet_schedule/tasklet_schedule.c
+++ tasklet_schedule/tasklet_schedule.c
@@ -2,17 +2,23 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
+#include <linux/preempt.h>
 
 
 void tasklet_handler(unsigned long data)
 {
 	pr_info("%s work\n", __func__);
+	pr_info("tasklet: in_interrupt(): 0x%0lx\n", in_interrupt());
+	pr_info("tasklet: in_irq(): 0x%0lx\n", in_irq());
+	pr_info("tasklet: in_softirq(): 0x%0lx\n", in_softirq());
+	pr_info("tasklet: in_nmi(): 0x%0lx\n", in_nmi());
 }
 
 DECLARE_TASKLET(my_tasklet, tasklet_handler, 0);
 
 static int __init hello_init(void)
 {
+	pr_info("module init : in_interrupt(): 0x%0lx\n", in_interrupt());
 	tasklet_schedule(&my_tasklet);
 	return 0;
 }
